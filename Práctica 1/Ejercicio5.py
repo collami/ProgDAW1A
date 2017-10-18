b085c110fa14752ebafa42c472d67dd3ebcae6a6
collami <32308543+collami@users.noreply.github.com>
2017-10-18 20:42:36 +0200

    Add files via upload

---
+++ Práctica 1/Ejercicio5.py
@@ -0,0 +1,10 @@
+﻿print 'Averiguar el mayor de dos números dados'
+N1=(input('Introduce el primer número:'))
+N2=(input('Introduce el segundo número:'))
+if N1>=N2:
+    if N1==N2:
+        print 'Los dos números son iguales'
+    else:
+        'El',N1,'es el número mayor'
+else:
+    print 'El',N2,'es el número mayor'
