61c63af1574ada6eaeadb075a0a4ef093dc77941
collami <32308543+collami@users.noreply.github.com>
2017-10-19 20:48:50 +0200

    Add files via upload

---
+++ Práctica 4/Ejercicio 3.py
@@ -0,0 +1,11 @@
+print 'Números pares e impares de un rango'
+
+n1=int (input('Introduce el valor inicial del rango:'))
+                 
+n2=int (input('Introduce el valor final del rango:'))
+
+r=n2-n1
+                 
+for i in range(n1,n2):
+    i=i+1
+    print i,
