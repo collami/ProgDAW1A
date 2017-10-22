e5bfcbe82821999028fa7c13c6fb25b988f36034
collami <32308543+collami@users.noreply.github.com>
2017-10-22 18:58:52 +0200

    Delete Ejercicio 6.py

--- Práctica 4/Ejercicio 6.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: cp1252 -*-
-print 'Dibujando un triángulo dando su altura'
-a=input('Introduce la altura del trángulo:')
-a=abs(a)
-for i in range(a):
-    for j in range(i+1):
-        print '*',
-    print
