b6b43458b915cc5ed34c6db2a87e08cca9e4bd79
collami <32308543+collami@users.noreply.github.com>
2017-10-22 18:58:36 +0200

    Delete Ejercicio 4.py

--- Práctica 4/Ejercicio 4.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: cp1252 -*-
-print 'Cálculo del factorial de un número dado'
-a=int(input('Introduce un número entero positivo:'))
-a=abs(a)
-j=1
-for i in range(a,1,-1):
-    j=j*i
-print 'El factorial de',a,'es:',j,
-   
