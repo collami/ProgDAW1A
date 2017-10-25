c82d445e4c4506ea4a1182e93b8d64f318397c2a
collami <32308543+collami@users.noreply.github.com>
2017-10-25 21:20:52 +0200

    Add files via upload

---
+++ Práctica 5/Ejercicio 1 v.2.py
@@ -0,0 +1,12 @@
+# -*- coding: cp1252 -*-
+print 'Escribiendo palabras en una lista'
+a=raw_input('Introduce una palabra:')
+lista=[]
+while a!="":
+    lista.append(a)   
+    a=raw_input('Introduce una palabra:')
+print lista
+    
+
+
+
---
+++ Práctica 5/Ejercicio 2.py
@@ -0,0 +1,12 @@
+# -*- coding: cp1252 -*-
+print 'Escribiendo números en una lista'
+a=raw_input('Introduce un número:')
+lista=[]
+while a!='':
+    lista.append(float(a))
+    a=raw_input('Introduce un número:')
+print lista
+    
+
+
+
