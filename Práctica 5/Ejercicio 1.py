ccd9fd7526c631b60dd647a3dfed4827e2a7b57a
collami <32308543+collami@users.noreply.github.com>
2017-10-26 13:05:12 +0200

    Add files via upload

--- Práctica 5/Ejercicio 1.py
+++ Práctica 5/Ejercicio 1.py
@@ -3,7 +3,7 @@ print 'Escribiendo palabras en una lista'
 a=raw_input('Introduce una palabra:')
 lista=[]
 while a!="":
-    lista=lista+[a]    
+    lista.append(a)   
     a=raw_input('Introduce una palabra:')
 print lista
     
